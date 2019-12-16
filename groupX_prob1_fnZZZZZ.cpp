94487aa18bee0f96101822d0ba747f99c64d0364
Trifon Trifonov <triffon@gmail.com>
2019-12-17 00:59:13 +0200

    Примерно решение на задача 1 и поправка на някои тестове

--- groupX_prob1_fnZZZZZ.cpp
+++ groupX_prob1_fnZZZZZ.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <stack>
 
 /***********************************************************************
   Задача 1. TBD
@@ -8,7 +9,48 @@
  РЕШЕНИЕ:
 ************************************************************************/
 
-bool valid(const std::string& s, unsigned& error);
+bool isLetter (char c) { return 'a' <= c && c <= 'z'; }
+bool isLetterQ(char c) { return isLetter(c) || c == '?'; }
+
+bool valid(const std::string& s, unsigned& error) {
+  unsigned& i = error;
+  std::stack<char> stack;
+  for(i = 0; i < s.size(); ++i)
+    if (s[i] == '(') {
+      if (stack.empty() || !isLetter(stack.top()))
+        return false;
+      stack.push(s[i]);
+    }
+    else if (isLetter(s[i])) {
+      if (!stack.empty() && stack.top() != '(')
+        return false;
+      stack.push(s[i]);
+    }
+    else if (s[i] == ',') {
+      if (stack.empty() || !isLetterQ(stack.top()))
+        return false;
+      stack.pop();
+      if (stack.empty() || stack.top() != '(')
+        return false;
+    }
+    else if (s[i] == ')') {
+      if (stack.empty() || !isLetterQ(stack.top()))
+        return false;
+      stack.pop();
+      if (stack.empty() || stack.top() != '(')
+        return false;
+      stack.pop();
+      if (stack.empty() || !isLetter(stack.top()))
+        return false;
+      stack.pop();
+      stack.push('?');
+    }
+    else return false;
+  if (stack.empty() || !isLetterQ(stack.top()))
+    return false;
+  stack.pop();
+  return stack.empty();
+}
 
 /***********************************************************************
  КРАЙ НА РЕШЕНИЕТО
@@ -20,7 +62,7 @@ bool valid(const std::string& s, unsigned& error);
 /***********************************************************************
   РАЗКОМЕНТИРАЙТЕ СЛЕДВАЩИЯ РЕД, ЗА ДА ВКЛЮЧИТЕ ТЕСТОВЕТЕ
 ************************************************************************/
-//#include "prob1_tests.h"
+#include "prob1_tests.h"
 
 int main ()
 {
