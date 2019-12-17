53ec101a8527279f4eaccc7b16ee6f2ca7bcabfc
Trifon Trifonov <triffon@gmail.com>
2019-12-17 15:11:54 +0200

    Примерно решение на задача 4 и поправка на тестовете

--- groupX_prob4_fnZZZZZ.cpp
+++ groupX_prob4_fnZZZZZ.cpp
@@ -10,13 +10,43 @@
  РЕШЕНИЕ: а)
 ************************************************************************/
 
-Node<char>* buildTree (const std::string& s);
+Node<char>* buildTree (const std::string& s) {
+  if (s.empty())
+    return nullptr;
+
+  unsigned mid = s.size() / 2;
+  std::string L = s.substr(0, mid), R = s.substr(mid + 1);
+  return new Node<char>(s[mid], buildTree(L), buildTree(R));
+}
 
 /***********************************************************************
  РЕШЕНИЕ: б)
 ************************************************************************/
 
-std::string countMax(Node<char>* t, char c);
+void findMaxCount(Node<char>* t, char c, int count, std::string& s, int& max, std::string& maxPath) {
+  if (t == nullptr) {
+    if (count > max) {
+      max = count;
+      maxPath = s;
+    }
+  } else {
+    s.push_back(t->data);
+    if (t->data == c)
+      count++;
+    findMaxCount(t->left , c, count, s, max, maxPath);
+    findMaxCount(t->right, c, count, s, max, maxPath);
+    if (t->data == c)
+      count--;
+    s.pop_back();
+  }
+}
+
+std::string countMax(Node<char>* t, char c) {
+  std::string empty, result;
+  int max = 0;
+  findMaxCount(t, c, 0, empty, max, result);
+  return result;
+}
 
 /***********************************************************************
  КРАЙ НА РЕШЕНИЕТО
@@ -28,7 +58,7 @@ std::string countMax(Node<char>* t, char c);
 /***********************************************************************
   РАЗКОМЕНТИРАЙТЕ СЛЕДВАЩИЯ РЕД, ЗА ДА ВКЛЮЧИТЕ ТЕСТОВЕТЕ
 ************************************************************************/
-//#include "prob4_tests.h"
+#include "prob4_tests.h"
 
 int main ()
 {
